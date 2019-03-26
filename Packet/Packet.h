ac3a69af8e30fc5fc069ace7e4a85610e7a4609f
RodrigoHolztrattner <rodrigoholztrattner@gmail.com>
2019-03-25 21:48:46 -0700

    Export the ResourceExternalConstructor and the EditableResourceReferencePtr classes

--- Packet/Packet.h
+++ Packet/Packet.h
@@ -29,25 +29,29 @@ PacketNamespaceBegin(Packet)
 // GLOBAL //
 ////////////
 
-typedef __development__Packet::PacketResource				Resource;
-typedef __development__Packet::PacketResourceInstance		ResourceInstance;
-typedef __development__Packet::PacketResourceFactory		ResourceFactory;
+typedef __development__Packet::PacketResource				     Resource;
+typedef __development__Packet::PacketResourceInstance		     ResourceInstance;
+typedef __development__Packet::PacketResourceFactory		     ResourceFactory;
+typedef __development__Packet::PacketResourceExternalConstructor ResourceExternalConstructor;
 
-typedef __development__Packet::PacketResourceManager		ResourceManager;
-typedef __development__Packet::PacketSystem					System;
+typedef __development__Packet::PacketResourceManager		     ResourceManager;
+typedef __development__Packet::PacketSystem					     System;
 
-typedef __development__Packet::OperationMode				OperationMode;
-typedef __development__Packet::ReferenceFixer				ReferenceFixer;
-typedef __development__Packet::Hash							Hash;
-typedef __development__Packet::PacketResourceData			ResourceData;
-typedef __development__Packet::PacketResourceBuildInfo		ResourceBuildInfo;
-typedef __development__Packet::ThreadIndexRetrieveMethod	ThreadIndexRetrieveMethod;
+typedef __development__Packet::OperationMode				     OperationMode;
+typedef __development__Packet::ReferenceFixer				     ReferenceFixer;
+typedef __development__Packet::Hash							     Hash;
+typedef __development__Packet::PacketResourceData			     ResourceData;
+typedef __development__Packet::PacketResourceBuildInfo		     ResourceBuildInfo;
+typedef __development__Packet::ThreadIndexRetrieveMethod	     ThreadIndexRetrieveMethod;
 
 template <typename InstanceClass>
-using ResourceInstancePtr = __development__Packet::PacketResourceInstancePtr<InstanceClass>;
+using ResourceInstancePtr           = __development__Packet::PacketResourceInstancePtr<InstanceClass>;
 
 template <typename ResourceClass>
-using ResourceReferencePtr = __development__Packet::PacketResourceReferencePtr<ResourceClass>;
+using ResourceReferencePtr          = __development__Packet::PacketResourceReferencePtr<ResourceClass>;
+
+template <typename ResourceClass>
+using EditableResourceReferencePtr  = __development__Packet::PacketEditableResourceReferencePtr<ResourceClass>;
 
 // Packet
 PacketNamespaceEnd(Packet)
